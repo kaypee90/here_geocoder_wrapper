2c781babf8beaa85faa01831777e5786be915f3a
unknown <kaypee90@yahoo.com>
2020-01-03 11:10:27 +0000

    Document search method

--- heregeocoder/GeocoderApi.py
+++ heregeocoder/GeocoderApi.py
@@ -6,6 +6,15 @@ class GeocoderApi:
         self.base_url = "https://geocoder.ls.hereapi.com/6.2/geocode.json"
     
     def search(self, search_text):
+        """Summary or Description of the Function
+
+        Parameters:
+        search_text (int): address being searched for
+
+        Returns:
+        object: python request library response object
+
+       """
         response = requests.get(
             self.base_url,
             params={
